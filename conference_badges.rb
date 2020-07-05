cc887aead3109e82e09fd21df6cb5463e8ce23e1
Ynda Jas <yndajas@gmail.com>
2020-07-05 03:37:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,15 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms (speakers)
+  next_room = 1
+  speakers.each_with_index do |name,index|
+    message = "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    next_room += 1
+    message
+  end
+end
+
+def assign_rooms (speakers)
   room_assignments = []
   speakers.each_with_index do |name,index|
     room_assignments << "Hello, #{name}! You'll be assigned to room #{index+1}!"
