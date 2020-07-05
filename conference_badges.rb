52d09bc5cc98be12474db75a5685ecb8de787337
Ynda Jas <yndajas@gmail.com>
2020-07-05 03:29:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,15 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  array.collect {|name|badge_maker(name)}
+def batch_badge_creator(speakers)
+  speakers.collect {|name|badge_maker(name)}
+end
+
+def assign_rooms (speakers)
+  next_room = 1
+  speakers.collect do |name|
+    message = "Hello, #{name}! You'll be assigned to room #{next_room}!"
+    next_room += 1
+    message
+  end
 end
